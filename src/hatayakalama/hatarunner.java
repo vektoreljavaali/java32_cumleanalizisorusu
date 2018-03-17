7063df1cc69e55a4df29236adb7bdc22d006b4e8
temelt <tanertemel@outlook.com>
2018-03-17 16:41:14 +0300

    son.

--- src/hatayakalama/hatarunner.java
+++ src/hatayakalama/hatarunner.java
@@ -1,10 +1,40 @@
 package hatayakalama;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class hatarunner {
 
 	
 	 public static void main(String[] args) {
 		
+		 Scanner sc = new Scanner(System.in);
+		 
+		 System.out.print("bir sayý giriniz...: ");
+		 
+		 String[] ifade = {"1","2"};
+		
+		try {
+			 int sayi1 = sc.nextInt();
+			 System.out.println(ifade[-1]);
+			 int toplam = sayi1/0;
+			 System.out.println("Baþarýlý bir þekilde rakam alýndý");
+		} catch (InputMismatchException e) {
+			System.out.println("bir hata ile karþýlaþýldý.");
+			System.out.println(e.toString());
+		} catch(ArithmeticException ex) {
+			System.out.println("Aritmetik bir hata oldu..:"+ex.toString());
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Bilinmeyen bir hata oldu..:"+e.toString());
+		}finally {
+			System.out.println("Her halükarda çalýþýrým....");
+		}
+		 
+		
 	}
 	
 }
+
+
+
+
